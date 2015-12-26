3b2e8f39a997947557a868974b70b257d46e6bed
Diego <diego.pereirasilva@live.com>
2015-12-25 23:49:04 -0200

    Aplicacao com fotos mmais populares do instagram

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
-// grab express
+
+
+// GRAB THE PACKAGES/VARIABLES WE NEED
+// ==================================================
 var express = require('express');
-// create an express app
-var app = express();
-// create an express route for the home page
-// http://localhost:8080/
-app.get('/', function(req, res) {
-res.sendfile('index.html');
+var app     = express();
+var ig      = require('instagram-node').instagram();
+
+// CONFIGURE THE APP
+// ==================================================
+// tell node where to look for site resources
+app.use(express.static(__dirname + '/public'));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// configure instagram app with client_id
+ig.use({
+    client_id: 'e0e51c60672c4f09abe28c46c71a3a7a',
+    client_secret: 'db11c575a8ae4f1aa90a03ba1d1345d8'
 });
-// start the server on port 8080
+
+// SET THE ROUTES
+// ===================================================
+// home page route - popular images
+app.get('/', function(req, res) {
+
+    // use the instagram package to get popular media
+    ig.media_popular(function(err, medias, remaining, limit) {
+        // render the home page and pass in the popular images
+        res.render('pages/index', { grams: medias });
+    });
+
+});
+
+// START THE SERVER
+// ==================================================
 app.listen(8080);
-// send a message
-console.log('Server has started!');
+console.log('App started! Look at http://localhost:8080');
